f16551b88f1c78a0a6544296ad13c8a43e83302f
Alec <alec4nandes@gmail.com>
2023-11-25 12:46:20 -0800

    check download status

--- public/scripts/reloader.js
+++ public/scripts/reloader.js
@@ -1,3 +1,5 @@
+const IS_DEVELOPMENT = false;
+
 // PWA can get stale when active in the background after opening
 // from a device's home screen, so reload every hour in case
 // there are page updates.
@@ -5,12 +7,24 @@ setTimeout(() => {
     window.location.reload();
 }, 1000 * 60 * 60);
 
+const isDownloaded =
+    IS_DEVELOPMENT || !!window.matchMedia("(display-mode: standalone)").matches;
+
 // when restarting the app, not just refreshing
-if (!pageIsReloaded()) {
-    document.querySelector("#welcome").style.display = "flex";
-    setTimeout(() => {
-        window.location.reload();
-    }, 2000);
+if (pageIsReloaded()) {
+    if (!isDownloaded) {
+        showWelcome();
+        showInstallInstructions();
+    }
+} else {
+    showWelcome();
+    if (isDownloaded) {
+        setTimeout(() => {
+            window.location.reload();
+        }, 2000);
+    } else {
+        showInstallInstructions();
+    }
 }
 
 function pageIsReloaded() {
@@ -22,4 +36,17 @@ function pageIsReloaded() {
         return isReload;
     }
     return false;
+}
+
+function showWelcome() {
+    document.querySelector("#welcome").style.display = "block";
+}
+
+function showInstallInstructions() {
+    document.querySelector("#install").innerHTML = `
+        You must download this web app to your device
+        to receive push notification reminders.
+        Follow these downloading directions:
+        ...
+    `;
 }
